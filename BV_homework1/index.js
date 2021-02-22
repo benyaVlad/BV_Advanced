dabfa3a8ef57bdbb59e0f950860c97fcbc310b79
Vlad Benidovskyi <BVstomat@gmail.com>
2021-02-22 15:00:24 +0200

    New changes in strings 18-20, 39-50

--- BV_homework1/index.js
+++ BV_homework1/index.js
@@ -7,45 +7,42 @@ console.log("Максимальна ціна:", Math.max(priceAspirin, priceClar
 
 console.log("Мінімальна ціна:", Math.min(priceAspirin, priceClaritin, priceSupradyn));
 
-const sumPrices = priceAspirin + priceClaritin + priceSupradyn;
-console.log("Вартість усіх товарів:", sumPrices);
+const sumDrugPrices = priceAspirin + priceClaritin + priceSupradyn;
+console.log("Вартість усіх товарів:", sumDrugPrices);
 
 const floorSumPrices = Math.floor(priceAspirin) + Math.floor(priceClaritin) + Math.floor(priceSupradyn);
 console.log("Округлена вартість усіх товарів у меншу сторону:", floorSumPrices);
 
-console.log("Округлена вартість усіх товарів до сотень:", Math.round(sumPrices / 100) * 100);
+console.log("Округлена вартість усіх товарів до сотень:", Math.round(sumDrugPrices / 100) * 100);
 
-if (floorSumPrices % 2 === 0){
-    console.log("Округлена вартість усіх товарів - парне число:", true);
-} else {
-    console.log("Округлена вартість усіх товарів - непарне число:", false);
-}
+floorSumPrices % 2 === 0 ? console.log("Округлена вартість усіх товарів - парне число:", true) : console.log("Округлена вартість усіх товарів - непарне число:", false);
+
 
 const sumClient = 500;
-const sumRest = sumClient - sumPrices;
+const sumRest = sumClient - sumDrugPrices;
 console.log("Сума решти:", sumRest);
 
 
-const sumMedian = sumPrices / 3;
+const sumMedian = sumDrugPrices / 3;
 console.log("Cереднє значення цін, округлене до другого знаку після коми:", sumMedian.toFixed(2));
 
-const saleSumPrices = Math.floor(Math.random() * 101);
-const toPaySumPrices = (sumPrices - saleSumPrices);
-console.log("Снижка:", saleSumPrices);
-console.log("До сплати:", toPaySumPrices.toFixed(2));
-const sumEarnings = sumPrices / 2 - saleSumPrices;
-console.log("Чистий прибуток:", sumEarnings.toFixed(2));
+const discount = Math.floor(Math.random() * 101).toFixed(2);
+const toPaySumPrices = (sumDrugPrices - ((sumDrugPrices/100) * discount)).toFixed(2);
+console.log("Снижка:", discount, "%");
+console.log("До сплати:", toPaySumPrices);
+const sumEarnings = ((sumDrugPrices.toFixed(2)/2) - (sumDrugPrices.toFixed(2)-toPaySumPrices));
+console.log("Чистий прибуток:", sumEarnings);
 
 const soloString = `
-Максимальна ціна: ${Math.max(priceAspirin, priceClaritin, priceSupradyn)}
-Мінімальна ціна: ${Math.min(priceAspirin, priceClaritin, priceSupradyn)}
-Вартість усіх товарів: ${sumPrices}
-Округлена вартість усіх товарів у меншу сторону: ${floorSumPrices}
-Округлена вартість усіх товарів до сотень: ${Math.round(sumPrices / 100) * 100}
-Округлена вартість усіх товарів - парне число: ${true}
-Сума решти: ${sumRest}
-Cереднє значення цін, округлене до другого знаку після коми: ${sumMedian.toFixed(2)}
-Снижка: ${saleSumPrices}
-До сплати: ${toPaySumPrices.toFixed(2)}
-Чистий прибуток: ${sumEarnings.toFixed(2)}`;
+Максимальна ціна: ${Math.max(priceAspirin, priceClaritin, priceSupradyn)}\n
+Мінімальна ціна: ${Math.min(priceAspirin, priceClaritin, priceSupradyn)}\n
+Вартість усіх товарів: ${sumDrugPrices}\n
+Округлена вартість усіх товарів у меншу сторону: ${floorSumPrices}\n
+Округлена вартість усіх товарів до сотень: ${Math.round(sumDrugPrices / 100) * 100}\n
+Округлена вартість усіх товарів - парне число: ${true}\n
+Сума решти: ${sumRest}\n
+Cереднє значення цін, округлене до другого знаку після коми: ${sumMedian.toFixed(2)}\n
+Снижка: ${discount} %\n
+До сплати: ${toPaySumPrices}\n
+Чистий прибуток: ${sumEarnings}`;
 console.log(soloString);
